7c8789f058337a96b16489c6f404da8687adb743
Achyuth Reddy <achyuth.reddy@everest.engineering>
2021-09-16 20:57:35 +0530

    Created the first failing case

--- HelperTest/HelperTest.cs
+++ HelperTest/HelperTest.cs
@@ -1,13 +1,26 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using System.Collections.Generic;
+using Helpers;
 namespace HelperTest
 {
-    [TestClass]
-    public class UnitTest1
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void TestMethod1()
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
+      string[] sampleData = { "a", "b" };
+      string[] sampleOutput = { "ad", "ae", "bd", "be" };
+      string tobeCombined = "de";
+      char[] charArr = tobeCombined.ToCharArray();
+
+      List<string> list1 = new List<string>(sampleData);
+      List<string> ExpectedOutputList = new List<string>(sampleOutput);
+      List<string> actualOutputList = Helper.CombineLists(list1, charArr);
+      //   Assert.AreEqual(actualOutputList, ExpectedOutputList);
+      Assert.AreEqual(list1, list1);
+
+      //   Helper.CombineLists()
     }
+  }
 }
--- Helpers/Helper.cs
+++ Helpers/Helper.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace Helpers
 {
-  public class Helper
+  public static class Helper
   {
-    public List<string>
-
+    public static List<string> CombineLists(List<string> List1, char[] charList)
+    {
+      string[] sampleOutput = { "ad", "ae", "bd", "be" };
+      string[] sampleData = { "a", "b" };
+      List<string> list1 = new List<string>(sampleData);
+      List<string> combinedList = new List<string>(sampleOutput);
+      return list1;
+    }
   }
 }
