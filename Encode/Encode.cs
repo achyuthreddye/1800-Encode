4e82b7a923a7d485d7a32d9b446163ab173fb49d
Achyuth Reddy <achyuth.reddy@everest.engineering>
2021-09-17 14:50:29 +0530

    Implemented the first failing and passed condition

--- Encode/Encode.cs
+++ Encode/Encode.cs
@@ -1,8 +1,29 @@
-﻿using System;
-
+﻿using System.Collections.Generic;
+using Helpers;
 namespace Encode
 {
-    public class Class1
+  public class EncodePhone
+  {
+    // Helper.CombineListBasedOnDictionary();
+
+    static string[] dictionary = { "callme", "call", "ball", "apache", "hyundai", "zebra" };
+    static string[] sampleData = { "a", "b", "c" };
+    static string[] sampleOutput = { "ba", "ca" };
+    static string tobeCombined = "abc";
+    static char[] charArr = tobeCombined.ToCharArray();
+
+    static List<string> list1 = new List<string>(sampleData);
+    List<string> ExpectedOutputList = new List<string>(sampleOutput);
+
+    public List<string> enocdeDistionary()
     {
+
+      List<string> combinedList = Helper.CombineListBasedOnDictionary(list1, charArr, dictionary);
+
+
+      return combinedList;
+
     }
+
+  }
 }
--- EncodeTest/EncodeTest.cs
+++ EncodeTest/EncodeTest.cs
@@ -1,13 +1,21 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Encode;
+using System.Collections.Generic;
 
 namespace EncodeTest
 {
-    [TestClass]
-    public class UnitTest1
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void TestMethod1()
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
+      EncodePhone encode = new EncodePhone();
+      string[] sampleOutput = { "ba", "ca" };
+      List<string> ExpectedOutputList = new List<string>(sampleOutput);
+
+      CollectionAssert.AreEqual(encode.enocdeDistionary(), ExpectedOutputList);
+
     }
+  }
 }
