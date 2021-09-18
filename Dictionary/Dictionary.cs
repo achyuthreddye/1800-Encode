326bcc188f7d0dbccfc8caee8287c545af3106aa
Achyuth Reddy <achyuth.reddy@everest.engineering>
2021-09-18 19:14:49 +0530

    minor fixes

--- Dictionary/Dictionary.cs
+++ Dictionary/Dictionary.cs
@@ -1,17 +1,15 @@
-﻿
-namespace Dictionary
+﻿namespace Dictionary
 {
   public static class DictionaryData
   {
     public static string[] readDataFromTextFile()
     {
-      // string[] dictionary =
-      // { "callme", "call", "ball", "apache", "hyundai", "zebra" };
-      string[] lines = System.IO.File.ReadAllLines(@"/Users/achyuthreddy/Desktop/js projects/everest_coding_assignments/1800_coding/dictionary.txt");
+      string[] lines =
+          System
+              .IO
+              .File
+              .ReadAllLines(@"/Users/achyuthreddy/Desktop/js projects/everest_coding_assignments/1800_coding/dictionary.txt");
       return lines;
-
     }
   }
 }
-
-
