cac50a75bf808f5b8905081d122a0e85e58715e8
Achyuth Reddy <achyuth.reddy@everest.engineering>
2021-09-20 12:02:12 +0530

    Changed the if statements to the switch statements

--- ConsoleApp/ProcessUserCommands.cs
+++ ConsoleApp/ProcessUserCommands.cs
@@ -8,22 +8,26 @@ namespace ConsoleApp
   {
     public static void ProcessIn(string input)
     {
-      if (input == "encode")
+      switch (input)
       {
-        EncodePhone encode = new EncodePhone();
-        Console
-            .WriteLine("Please enter the phone number that has to encoded");
-        string inputPhNo = Console.ReadLine();
-        List<string> encodedWords = new List<string>();
-        encodedWords = encode.encodePhNo(inputPhNo);
-        foreach (string item in encodedWords)
-        {
-          Console.WriteLine(item);
-        }
-        if (encodedWords.Count == 0)
-        {
-          Console.WriteLine("there are no possible encoding for " + inputPhNo + "  in the dictionary. ");
-        }
+        case "encode":
+          {
+            EncodePhone encode = new EncodePhone();
+            Console
+                .WriteLine("Please enter the phone number that has to encoded");
+            string inputPhNo = Console.ReadLine();
+            List<string> encodedWords = new List<string>();
+            encodedWords = encode.encodePhNo(inputPhNo);
+            foreach (string item in encodedWords)
+            {
+              Console.WriteLine(item);
+            }
+            if (encodedWords.Count == 0)
+            {
+              Console.WriteLine("there are no possible encoding for " + inputPhNo + "  in the dictionary. ");
+            }
+            break;
+          }
       }
     }
   }
