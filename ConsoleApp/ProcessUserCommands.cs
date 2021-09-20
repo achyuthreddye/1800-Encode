c69bc418e32fad22a3b35c35255184925eb6b36e
Achyuth Reddy <achyuth.reddy@everest.engineering>
2021-09-20 12:52:04 +0530

    Implemented the wrapper for console app

--- ConsoleApp/ProcessUserCommands.cs
+++ ConsoleApp/ProcessUserCommands.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using Encode;
-using Dictionary;
-
 namespace ConsoleApp
 {
   public static class ProcessUserCommands
@@ -13,38 +8,17 @@ namespace ConsoleApp
       {
         case "encode":
           {
-            EncodePhone encode = new EncodePhone();
-            Console
-                .WriteLine("Please enter the phone number that has to encoded");
-            string inputPhNo = Console.ReadLine();
-            List<string> encodedWords = new List<string>();
-            encodedWords = encode.encodePhNo(inputPhNo);
-            foreach (string item in encodedWords)
-            {
-              Console.WriteLine(item);
-            }
-            if (encodedWords.Count == 0)
-            {
-              Console.WriteLine("there are no possible encoding for " + inputPhNo + "  in the dictionary. ");
-            }
+            Wrapper.EncodePhone();
             break;
           }
         case "dictionaryview":
           {
-            string[] dicData = DictionaryData.readDataFromTextFile();
-            Console.WriteLine("The words in the dictionary are");
-            foreach (string item in dicData)
-            {
-              Console.WriteLine(item);
-
-            }
+            Wrapper.viewDictionary();
             break;
           }
         case "addworddictionary":
           {
-            Console.WriteLine("Please enter the word that has to added to the dictionary");
-            string wordTobeAdded = Console.ReadLine();
-            DictionaryData.writeDatatoTextFile(wordTobeAdded);
+            Wrapper.addToDictionary();
             break;
           }
       }
---
+++ ConsoleApp/Wrapper.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using Dictionary;
+using Encode;
+
+namespace ConsoleApp
+{
+  public static class Wrapper
+  {
+    public static void EncodePhone()
+    {
+      EncodePhone encode = new EncodePhone();
+      Console
+          .WriteLine("Please enter the phone number that has to encoded");
+      string inputPhNo = Console.ReadLine();
+      List<string> encodedWords = new List<string>();
+      encodedWords = encode.encodePhNo(inputPhNo);
+      foreach (string item in encodedWords)
+      {
+        Console.WriteLine(item);
+      }
+      if (encodedWords.Count == 0)
+      {
+        Console
+            .WriteLine("there are no possible encoding for " +
+            inputPhNo +
+            "  in the dictionary. ");
+      }
+    }
+
+    public static void viewDictionary()
+    {
+      string[] dicData = DictionaryData.readDataFromTextFile();
+      Console.WriteLine("The words in the dictionary are");
+      foreach (string item in dicData)
+      {
+        Console.WriteLine(item);
+      }
+    }
+
+    public static void addToDictionary()
+    {
+      Console.WriteLine("Please enter the word that has to added to the dictionary");
+      string wordTobeAdded = Console.ReadLine();
+      DictionaryData.writeDatatoTextFile(wordTobeAdded);
+      Console.WriteLine("Added the word to the dictionary. Successfully!!!!");
+
+    }
+  }
+}
